523a5b628faecba8ac972547826df95a756db3bf
Kajalkanade9755 <74231333+Kajalkanade9755@users.noreply.github.com>
2023-01-12 14:25:50 +0530

    Create ModifyData.h

---
+++ ModifyData.h
@@ -0,0 +1,15 @@
+#ifndef MODIFYDATA_H
+#define MODIFYDATA_H
+
+class ModifyData
+{
+    public:
+        ModifyData();
+//void delete_line(const string& idNr,int PId,int EId,char Description[50],char date[80],int duration,char Depat[50],char Group[50]);
+ void CheckPId(int loginid);
+    protected:
+
+    private:
+};
+
+#endif // MODIFYDATA_H
